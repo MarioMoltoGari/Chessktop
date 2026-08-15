97aa39aa8dac661cd111ac595dfb6dfab98561b8
Mario <mmoltogari@gmail.com>
2026-08-15 12:39:54 +0200

    fix: arreglo de notas en pgn y preparación para subida a vercel

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,8 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
 
-  base: "/Chessktop/",
+  base:
+    process.env.VITE_DEPLOY_TARGET === "github"
+      ? "/Chessktop/"
+      : "/",
 });
